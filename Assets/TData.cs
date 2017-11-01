5e357b7ad4c061f67b99298dfdd4dd4c927459e8
PseudoscienceGames <Quade81@gmail.com>
2017-11-01 18:05:30 -0500

    Stuff

---
+++ Assets/TData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TData : MonoBehaviour
+{
+	public int size;
+	public Texture2D tex;
+	// Use this for initialization
+	void Start () {
+		tex = new Texture2D(size, size);
+		for(int x = 0; x < size; x++)
+		{
+			for (int y = 0; y < size; y++)
+			{
+				tex.SetPixel(x, y, new Color(0, Random.Range(0, 5) / 5f, 0, 1));
+			}
+		}
+		tex.filterMode = FilterMode.Point;
+		tex.Apply();
+		GetComponent<Renderer>().material.mainTexture = tex;
+		System.IO.File.WriteAllBytes(Application.dataPath + "/Test.png", tex.EncodeToPNG());
+		GetComponent<TMesh>().GenMesh(tex);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TMesh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TMesh : MonoBehaviour
+{
+	public GameObject test;
+
+	public void GenMesh(Texture2D tex)
+	{
+		for(int x = 0; x < tex.width; x++)
+		{
+			for (int y = 0; y < tex.height; y++)
+			{
+				int h = Mathf.RoundToInt(tex.GetPixel(x, y).g * 5f);
+				float fZ = y;
+				if (x % 2 == 0)
+					fZ += 0.5f;
+				Instantiate(test, new Vector3(x, h / 2f, fZ), Quaternion.identity);
+			}
+		}
+	}
+}
