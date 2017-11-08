081712f5bd9960adf71344464b9776e639856624
PseudoscienceGames <quade.81@gmail.com>
2017-11-07 19:02:26 -0600

    move

---
+++ Assets/Scripts/Team.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+	public Material mat;
+	public List<Unit> units = new List<Unit>();
+	public GameObject unit;
+
+	private void Start()
+	{
+		for(int i = 1; i <= 3; i++)
+		{
+			Instantiate(unit);
+		}
+	}
+
+}
